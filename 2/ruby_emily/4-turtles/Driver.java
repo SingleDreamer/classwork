f61349f5e3f75c05f1f8b5678fa3fdbe73fe0bb6
Emily Ruby <emilyruby19@gmail.com>
2013-09-29 10:04:13 -0400

    turtles

---
+++ 2/ruby_emily/4-turtles/Driver.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String args[]){
+	Turtle t;
+	t=new Turtle();
+	System.out.println(t.getName("Jamie"));
+	t.set
+    }
+}
+
