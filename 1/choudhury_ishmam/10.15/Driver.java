f84d274b3e4ded654eaa6e6d6131e062ca3739d1
Ishmam Choudhury <i.choudhury9@gmail.com>
2013-10-15 08:38:56 -0400

    Classwork

---
+++ 1/choudhury_ishmam/10.15/Driver.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+		
+	public static void main (String[] args) {
+	
+	/*
+	int i = 9;
+	for (int i = 0 ; i < 10 ; i++) {
+		System.out.println(i);
+	}
+		System.out.println(i);
+		
+	}
+	*/ 
+		
+	
+	/* 
+	int i = 100;
+	for (int i = 0 ; i < 10 ; i++) {
+		System.out.println(i);
+	}
+		System.out.println(i);
+		
+	}
+	*/
+	
+	//printing a string backwards
+	
+	//given a string x = "hello" ; returns "olleh" then prints
+	
+	x = "hello";
+	
+	//for each character, append last charac to the end of the new string
+	// string = string + nextChar
+	
+}
