cb41d3a1297e5ca404436483bf71492b0f9cca53
IsaacGluck <isaacbgluck@gmail.com>
2013-10-16 18:37:41 -0400

    class

--- 3/gluck_isaac/ForLoops/ForLoop.java
+++ 3/gluck_isaac/ForLoops/ForLoop.java
@@ -3,13 +3,31 @@ import java.io.*;
 
 public class ForLoop {
 
-    public int isPrime(int n) {
+    public boolean isPrime(int n) {
 	int test = n-1;
-	for (n=n;test > 1;test--) {
+	for (test = n-1;test > 1;test--) {
 	    if (n%test==0)
 		return false;
 	}
 	return true;
     }
 
+    public String reverse(String s) {
+	int length = s.length();
+	String result = "";
+	for(int i = length -1; i>=0; i--)
+	    result = result + s.substring(i, i+1);
+	return result;
+    }
+
+    public String table(int n, int m) {
+	String result = "";
+	for(int i=0;i<=n;i++) {
+	    result = result + i;
+	    for (j=1;j<=i*n;j++)
+		result = result + j;
+	    
+	}
+	result = result + "\n";
+    }
 }
