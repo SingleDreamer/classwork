72e2478b2607c4b7010de427d1800979b4b2fbbf
George Vorobyev <quaffle97@gmail.com>
2013-11-01 13:16:01 -0400

    created folder for pd2 group1

---
+++ Stuyablo/2/group1/Character.java
@@ -0,0 +1,29 @@
+public class Character {
+   
+	protected String name;
+	protected int health;
+
+	protected int dex,str,intel;
+    
+
+	public Character() {
+		name="Barry";
+		health=20;
+	}
+
+	    
+	public Character(String name) {
+		this.name = name;
+		this.health=20;
+	}
+	    
+	public String toString() {
+		return name;
+	}
+
+	public String attack() {
+		return "Generic attack";
+	}
+
+
+}
