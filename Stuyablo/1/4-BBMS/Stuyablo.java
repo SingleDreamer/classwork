80c5a96a5187a0f18025fe744d8f71bdec62e755
Sean Yip <Bouowmx@users.noreply.github.com>
2013-11-02 10:54:59 -0400

    Updated Stuyablo.java with random stat generation
    Additionally, changed some variable names to make them shorter or to make them follow Java variable naming conventions.

--- Stuyablo/1/4-BBMS/Stuyablo.java
+++ Stuyablo/1/4-BBMS/Stuyablo.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class Stuyablo {
+	public static boolean isInRange(int x, int a, int b) {return ((x >= a) && (x <= b));} //tests if a <= x <= b
+	
+	public static boolean isInRange_exclusive(int x, int a, int b) {return ((x > a) && (x < b));} //tests if a < x < b
+		
 	public static void main(String[] args) {
 		System.out.println("Welcome to");
 		System.out.println("  _________ __                      ___.   .__          ");
@@ -17,21 +21,21 @@ public class Stuyablo {
 		
 		//Name
 		System.out.print("Enter name: ");
-		boolean wait_for_input = true; //method to check input here is slightly different when actually looking for a specific input, rather than simply any input at all. Is there a more efficient method to do this?
+		boolean input = false; //method to check input here is slightly different when actually looking for a specific input, rather than simply any input at all. Is there a more efficient method to do this? Also, named !input 
 		String name = "";
-		while (wait_for_input) {
+		while (!input) {
 			name = (scanner.nextLine()).trim();
-			if (!(name.equals(""))) {wait_for_input = false;}
+			if (!(name.equals(""))) {input = true;}
 		}
 		System.out.println("Name: " + name + "\n");
 		
 		//Class
 		System.out.print("Select a class (0 - Random, 1 - Class 1, 2 - Class 2, 3 - Class 3): ");
-		wait_for_input = true;
+		input = false;
 		String _class = ""; //lol, too lazy to think of any creative name
-		while (wait_for_input) {
+		while (!input) {
 			_class = (scanner.nextLine()).trim();
-			if ((_class.equals("0")) || (_class.equals("1")) || (_class.equals("2")) || (_class.equals("3"))) {wait_for_input = false;} //is there a more efficient method to do this line?
+			if ((_class.equals("0")) || (_class.equals("1")) || (_class.equals("2")) || (_class.equals("3"))) {input = true;} //is there a more efficient method to do this line?
 		}
 		Random random = new Random();
 		if (_class.equals("0")) {_class = String.valueOf(random.nextInt(3) + 1);} //keep forgetting that it's equals() and not ==, as in Python
@@ -39,6 +43,31 @@ public class Stuyablo {
 		if (_class.equals("2")) {_class = "Class 2";}
 		if (_class.equals("3")) {_class = "Class 3";}
 		System.out.println("Class: " + _class + "\n");
+		
+		//Stats (Not class-specific)
+		System.out.println("Stats:");
+		String answer = "";
+		boolean yes = false;
+		int attribute1 = 0, attribute2 = 0, attribute3 = 0, attribute4 = 0;
+		while (!yes) {
+			attribute1 = random.nextInt(11) + 5;
+			System.out.println("Attribute 1: " + attribute1);
+			attribute2 = random.nextInt(11) + 5;
+			System.out.println("Attribute 2: " + attribute2);
+			while (!(isInRange(attribute1 + attribute2 + attribute3, 25, 39))) {attribute3 = random.nextInt(11) + 5;}
+			System.out.println("Attribute 3: " + attribute3);
+			attribute4 = 40 - (attribute1 + attribute2 + attribute3);
+			System.out.println("Attribute 4: " + attribute4);
+			
+			System.out.print("Accept these stats (y/n)? ");
+			input = false;
+			while (!input) {
+				answer = (scanner.nextLine()).trim();
+				if ((answer.equals("y")) || (answer.equals("n"))) {input = true;}
+			}
+			if (answer.equals("y")) {yes = true;}
+			System.out.println("");
+		}
 		
 		//
 		System.out.println("\nProgram terminated.");
