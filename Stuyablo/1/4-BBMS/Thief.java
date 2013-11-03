b4f0955510b1bb7ba355944916b20a55fa6eb7a5
Ben <brothman7000@gmail.com>
2013-11-03 17:15:32 -0500

    More edits

---
+++ Stuyablo/1/4-BBMS/Thief.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Thief extends Character {
+
+    public Thief () {
+    }
+
+    public Thief (String name) {
+
+	super (name, "Thief");
+
+    }
+
+}
+
+
+
