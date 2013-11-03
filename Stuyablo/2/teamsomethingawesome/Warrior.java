cd93b97d82cc6c7fd3d6192b6a9616eef7343933
Richard Zhan <lilfirebolt@gmail.com>
2013-11-03 14:12:46 -0500

    Attack method added and basic Warrior class

--- Stuyablo/2/teamsomethingawesome/Warrior.java
+++ Stuyablo/2/teamsomethingawesome/Warrior.java
@@ -1,2 +1,10 @@
 public class Warrior extends Character{
+    public Warrior(){//Richard added
+	health = 25;
+	maxhealth = 25;
+	dexterity = 5;
+	strength = 5;
+	intelligence = 5;
+	name = "Arthur";
+	charClass = "Warrior";
 }
