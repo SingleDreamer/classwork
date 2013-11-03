1aa431e7b3afe9323cd986aac6cc14107de6f53b
tdiep26 <timd33@gmail.com>
2013-11-03 17:47:27 -0500

    Added Character

---
+++ Stuyablo/3/Group8/Character.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Character {
+    protected String Name;
+    protected int Health;
+    protected int Mana;
+    protected int Strength;
+    protected int Intelligence;
+    protected int Dexterity;
+    protected int Level;
+
+    public Character(String name, int Level) {
+	Name = name;
+	Level = level;
+	Strength = 8;
+	Intelligence = 8;
+	Dexterity = 8;
+	Health = 20;
+    }
+    
+}
+	
