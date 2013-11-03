e25f6650bdaba0610c66d7c51df31441f799eae8
EmileJB <dr.darklord1228@gmail.com>
2013-11-03 03:37:14 -0500

    Created character constructor that allows for user input or randomized input

--- Stuyablo/1/Group_2/Files/Character.java
+++ Stuyablo/1/Group_2/Files/Character.java
@@ -14,28 +14,84 @@ public class Character {
 
     private Random r = new Random();
 
-    protected void init(String name, int health, int m, int i, int d, int s, int e, double x, double y) {
+    protected void init(String name, int health, int m, int i, int d, int s, int e, double x, double y, int base) {
 	this.name = name;
 	this.health=health;
 	maxHealth = m;
-	intelligence = i;
-	dexterity = d;
-	strength = s;
+	intelligence = i + base;
+	dexterity = d + base;
+	strength = s + base;
 	experience = e;
 	this.x = x;
 	this.y = y;
     }
     
     public Character() {
-	init("No Name",50,100,8,8,8,0,0,0);
+	init("No Name",50,100,8,8,8,0,0,0,8);
     } 
     public Character(String name) {
-	init(name,50,100,8,8,8,0,0,0);
+	init(name,50,100,8,8,8,0,0,0,8);
     }
-    public Character(String name,int di,int dd,int ds) {
-	init(name,50,100,di+8,dd+8,ds+8,0,0,0);
+    public Character(String name,int di,int dd,int ds,int limit,int db,boolean pc,boolean preset) {
+	if (preset) {
+		init(name,50,100,di,dd,ds,0,0,0,db);}
+	else {
+	boolean sure = false;
+	if (pc) {
+	di = 0;
+	dd = 0;
+	ds = 0;
+	int total = 0;
+	while (!sure || total >= limit || total < 0) {
+	if (total >= limit || total < 0)
+		System.out.println("Stats invalid. Try again");		
+	di = 0;
+	dd = 0;
+	ds = 0;
+	System.out.print("What is your name? ");
+	Scanner scn = new Scanner(System.in);
+	name = scn.nextLine();
+	System.out.println("Now time to enter your stats. Remember that you get 8 points in every stat, and can put 8 extra points wherever.");
 
-    }
+	System.out.print("Enter desired extra intelligence points: ");
+	Scanner sci = new Scanner(System.in);
+	di = sci.nextInt();
+	System.out.println(" ");
+
+	System.out.print("Enter desired extra dexterity points: ");
+	Scanner scd = new Scanner(System.in);
+	dd = scd.nextInt();
+	System.out.println(" ");
+
+	System.out.print("Enter desired extra strength points: ");
+	Scanner scs = new Scanner(System.in);
+	ds = scs.nextInt();
+	System.out.println(" ");
+
+	System.out.println("Name: " + name + " Int: " + di + " Dex: " + dd + " Str: " + ds);
+	System.out.println("Are these okay y/n?"); 
+	Scanner sc = new Scanner(System.in);
+	if (sc.nextLine().equals("y"))
+		sure = true;
+	else if (sc.nextLine().equals("n"))
+		sure = false;
+	while (!sure && !sc.nextLine().equals("n")) {
+	System.out.println("Please enter y for yes or n for no");
+	sc = new Scanner(System.in);
+	if (sc.nextLine().equals("y"))
+		sure = true;
+	else if (sc.nextLine().equals("n"))
+		sure = false;
+	total = di + dd + ds; }}
+	System.out.println("Your Character " + name + " has been created!!"); }
+	else if (!pc) {
+ 	Random r = new Random();
+	di = r.nextInt(limit + 1);
+	dd = r.nextInt(limit - di + 1);
+	ds = limit - di - dd;}	
+	init(name,50,100,di,dd,ds,0,0,0,db);
+
+    }}
 
     public double distance(Character other){
 	return Math.sqrt(((other.x - x)*(other.x - x))+((other.y - y)*(other.y - y)));
--- Stuyablo/1/Group_2/Files/Driver.java
+++ Stuyablo/1/Group_2/Files/Driver.java
@@ -1,5 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
+	Character c1 = new Character("Name",3,3,2,8,8,false,false);
+	System.out.println(c1.name);
+	System.out.println(c1.intelligence);
+	System.out.println(c1.dexterity);
+	System.out.println(c1.strength);
 	Wizard wi = new Wizard("tom");
 	System.out.println(wi);
 
