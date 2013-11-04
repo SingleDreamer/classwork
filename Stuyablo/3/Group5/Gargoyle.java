164cd7bc1b74c407ddb0da5644df4e75e3507a8d
Angela <angela97lin@gmail.com>
2013-11-04 00:19:00 -0500

    added stuff

--- Stuyablo/3/Group5/Gargoyle.java
+++ Stuyablo/3/Group5/Gargoyle.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Gargoyle extends Character {
 
     public Gargoyle(){
+	//this is a constructor that sets up stats for gargoyle, a NPC! (randomly, of course)
 	Random r = new Random();
 	int strengthAdd = r.nextInt(7);
 	int dexAdd = 8 - strengthAdd;
