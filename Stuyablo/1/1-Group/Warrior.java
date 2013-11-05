e1295747d6a4086795c36db455e6adc723c22d72
Emma Lou <louemma2@gmail.com>
2013-11-05 15:33:41 -0500

    ---

--- Stuyablo/1/1-Group/Warrior.java
+++ Stuyablo/1/1-Group/Warrior.java
@@ -2,59 +2,75 @@ import java.io.*;
 import java.util.*;
 
 public class Warrior extends Character{
- 
+    
     public Warrior(String name){
-	super(name);
+        super(name);
     }
 
    
     
     public int attack (Character other){
-	//will add complicated x and y coor stuff later
-	/*attacks include hitting with:
-	  hammer(close range)
-	  sword(med range)
-	  arrow(far range)*/
+        int[] dice1={1,2,3,4,5,6};
+        int[] dice2={1,2,3,4,5,6};
+        int[] dice3={1,2,3,4,5,6};
+        //will add complicated x and y coor stuff later
+        /*attacks include hitting with:
+          hammer(close range)
+          sword(med range)
+          arrow(far range)*/
 
-	System.out.println("Choose your attack:");
-	System.out.println("Press 1 to use your hammer");
-	System.out.println("Press 2 to use your sword");
-	System.out.println("Press 3 to use your bow and arrow");
+        System.out.println("Choose your attack:");
+        System.out.println("Press 1 to use your hammer");
+        System.out.println("Press 2 to use your sword");
+        System.out.println("Press 3 to use your bow and arrow");
 
-	Scanner sc=new Scanner(System.in);
-	int answer=sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int answer=sc.nextInt();
 
-	//Hammer time
-	if (answer == 1){    
-	    //if x,y coors are <2 or something (something small)
-	    System.out.println(this.name + " whacked " + other.name +" with a hammer!");
-	    return super.attack(other);
-	}
-	
+        //Hammer time
+        if (answer == 1) {
+            int a=(dice1[new Random().nextInt(dice1.length)]);
+            int b=(dice1[new Random().nextInt(dice1.length)]);
+            int c=(dice1[new Random().nextInt(dice1.length)]);
+            if ((a+b+c)<this.dexterity) {
+            //if x,y coors are <2 or something (something small)
+                System.out.println(this.name + " whacked " + other.name +" with a hammer!");
+            return super.attack(other);
+            }
+            else {
+                System.out.println(this.name + "'s attack failed!");
+            }
+        }
+     
+        //Swords at the ready!
+        else if (answer == 2){
+            int a=(dice1[new Random().nextInt(dice1.length)]);
+            int b=(dice1[new Random().nextInt(dice1.length)]);
+            int c=(dice1[new Random().nextInt(dice1.length)]);
+            if ((a+b+c)<this.dexterity) {
+                System.out.println(this.name + " slashed " + other.name + " with a sword!");
+                return super.attack(other);
+            }
+            else {
+               System.out.println(this.name + "'s attack failed!");
+            }
+        }
 
-	//Swords at the ready!
-	else if (answer == 2){
-	    System.out.println(this.name + " slashed " + other.name + " with a sword!");
-	    return super.attack(other);
-	}
-
-	//*pew pew*
-	else if (answer == 3){
-	    System.out.println(this.name + " shot " + other.name + " with a bow and arrow!");
-	    return super.attack(other);
-	}
-
-	    else 
-		return super.attack(other);
-
+        //*pew pew*
+        else if (answer == 3){
+            int a=(dice1[new Random().nextInt(dice1.length)]);
+            int b=(dice1[new Random().nextInt(dice1.length)]);
+            int c=(dice1[new Random().nextInt(dice1.length)]);
+            if ((a+b+c)<this.dexterity) {
+                System.out.println(this.name + " shot " + other.name + " with a bow and arrow!");
+                return super.attack(other);
+            }
+            else {
+                System.out.println(this.name + "'s attack failed!");
+            }
+        }
+        else {
+            System.out.println(this.name + "you, made a typo!");
+        }
     }
 }
-
-
-
-
-
-
-
-
-
