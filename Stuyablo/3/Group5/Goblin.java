48740fae9af9d229363b6f1acc56965d945dcb97
Angela <angela97lin@gmail.com>
2013-11-05 18:32:46 -0500

    goblin driver

---
+++ Stuyablo/3/Group5/Goblin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Goblin extends Character {
+  
+
+
+    public Goblin(){
+	//this is a constructor that sets up stats for gargoyle, a NPC! (randomly, of course)
+	Random r = new Random();
+	int strengthAdd = r.nextInt(3);
+	int dexAdd = 4 - strengthAdd;
+	strength = 4 + strengthAdd;
+	dexterity = 4 + dexAdd;
+	maxhealth = strength;
+	health = maxhealth;
+    }
+
+	   
+}
