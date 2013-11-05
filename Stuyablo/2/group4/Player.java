0e7a5e66c4098146b2f1166f49650e46b735f602
Andrew Fischer <afischer15@mac.com>
2013-11-04 20:51:34 -0500

    .

---
+++ Stuyablo/2/group4/Player.java
@@ -0,0 +1,19 @@
+###TO BE MERGED WITH CHARACTER
+
+import java.io.*;
+import java.util.*;
+
+public class Warrior extends Character {
+
+
+    public Warrior(String n){
+	name=n;
+	health = 8;
+	dexterity = 8;
+	strength = 8;
+	intelligence = 8;
+
+	
+    }
+
+}
