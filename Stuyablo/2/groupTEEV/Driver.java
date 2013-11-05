19ccac868966e9bb8e89f0285e80f61d8cbac7d4
TLiang518 <terranceliang01@gmail.com>
2013-11-05 18:04:19 -0500

    added LevelUp

--- Stuyablo/2/groupTEEV/Driver.java
+++ Stuyablo/2/groupTEEV/Driver.java
@@ -1,8 +1,13 @@
 public class Driver {
     public static void main(String[] args) {
-	Character c = new Player();
+	Player c = new Player();
 	System.out.print("\n" + c.getStatus());
 	c.setStat();
+        System.out.print("\n" + c.getStatus());
+	c.gainExp(100);
+	if (c.getExperience() >= 100){
+	    c.levelUp();
+	}
         System.out.print("\n" + c.getStatus());
     }
 }
