8be969975d8e60de4a3c5a5d7ef3c6e9893ca6b3
erickolbusz <erickolbusz@gmail.com>
2013-11-05 17:30:48 -0500

    Started Second NPC

---
+++ Stuyablo/3/Group6/GoblinMage.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class GoblinMage extends Character {
+
+    public GoblinMage) {
+	int rand1 = (int)(Math.random() * 4);
+	int leftover = 8-rand1;
+
+	dexterity = 8 + rand1;
+	strength = 8 + leftover;
+	health = strength;
+	intelligence = 13;
+
+	experience = 50;
+	gold = 100;
+	name = "Goblin Mage";
+	charClass = "NPC";
+    }
+    
+    public void die(Character other) {
+	other.gold = other.gold + gold;
+	other.experience = other.experience + experience;
+	String winmessage = String.format("You have defeated the %s and gained %s experience and %s gold!", name, experience, gold);
+	System.out.println(winmessage);
+    }
+
+    public void attack(Character other) {
+	int die1 = (int)(Math.random() * 7);
+	int die2 = (int)(Math.random() * 7);
+	int die3 = (int)(Math.random() * 7);
+	int roll = die1 + die2 + die3;
+	System.out.println("The " + name + " tried attacking you!");
+	
+	if (roll > dexterity) {
+	    //miss
+	    System.out.println("The " + name + " stuttered while casting its spell and accidentally summoned a bag of potato chips!");}
+	else {
+	    //hit
+	    System.out.println("The " + name + "showered you in fireballs!");
+	    //other.health = other.health - ((SOME AMOUNT))
+	}
+    }
+}
