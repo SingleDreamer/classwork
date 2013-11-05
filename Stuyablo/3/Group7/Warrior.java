0f24b5e177b063388f6e33d1464f2bde61b0ca7c
Michelle Chan <ellehcimx123@gmail.com>
2013-11-05 11:44:10 -0500

    Minor changes to Warrior

--- Stuyablo/3/Group7/Warrior.java
+++ Stuyablo/3/Group7/Warrior.java
@@ -8,27 +8,27 @@ public class Warrior extends Character {
 
     public Warrior(String n){
 	name = n;
-	dexterity = 8;
-	strength = 10;
-	health = 10;
+	dexterity = 10;
+	strength = 15;
+	//health = 10;
 	maxhealth = 50;
-	experience = 8;
+	experience = 10;
 	//temporary values; might need to add new variables
     }
 
     public void attack(Character other){
 	Random r = new Random();
-	int rollDie = r.nextInt(18);
+	int rollDie = r.nextInt(18)+1;
 	//rolling a die method
 
-	if (rollDie < strength){
+	if (rollDie < dexterity){
 	    //might include weapons later
 	    //basically, the chacter will be able to attack
 	    System.out.println(n + " attacked and hit the enemy!");
 	    experience = experience + 1;
-	    if (experience = 12){
-		health = health + 5;
-		experience = 0;
+	    if (experience = 15){
+		maxhealth = maxhealth + 5;
+		experience = 10;
 		//kind of like leveling up
 	    }
 	    else {
