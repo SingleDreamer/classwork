dcb79a398cf8eb13ebc7ad361cea8cf8c52ceaf6
Sam Kim <samkim319@gmail.com>
2013-11-05 17:25:08 -0500

    added Mr. Moran

---
+++ Stuyablo/2/group7/MrMoran.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class MrMoran extends Character {
+    public MrMoran(String name) {
+	this.name = name;
+	Random r = new Random();
+	int str, dex, intel;
+	str = r.nextInt(5);
+	dex = r.nextInt(5-str);
+	intel = 10 - str - dex;
+	strength = 10 + str;
+	dexterity = 6 + dex;
+	intelligence = 4 + intel;
+	maxhealth = strength;
+	health = maxhealth;
+	
+    }
+}
