03b011cf6fe715bf8933eaef59f783eeed780190
Jake Olenick <jakeolenick@gmail.com>
2013-11-05 17:44:33 -0500

    made small improvements

--- Stuyablo/1/5-Group/NPC.java
+++ Stuyablo/1/5-Group/NPC.java
@@ -3,12 +3,35 @@ import java.util.*;
 
 public class NPC extends Character{
     Random r = new Random();
+    public NPC (String name, double Pstr, double Pdex, double Pintl, double Pdef, int lvl){
+	this.name = name;
+	str = 8;
+	dex = 8;
+	intl = 8;
+	def = 8;
+	this.lvl = lvl;
+	for (int i = 0; i < (7+lvl); i++){
+	    if (r.nextDouble<Pstr){
+		str = str + 1;
+	    }
+	    else if (r.nextDouble<Pdex){
+		dex = dex + 1;
+	    }
+	    else if (r.nextDouble<Pintl){
+		intl = intl + 1;
+	    }
+	    else {
+		def = def + 1;
+	    }
+	}
+    }
     public NPC (String name, double Pstr, double Pdex, double Pintl, double Pdef){
 	this.name = name;
 	str = 8;
 	dex = 8;
 	intl = 8;
 	def = 8;
+	this.lvl = 1;
 	for (int i = 0; i < 8; i++){
 	    if (r.nextDouble<Pstr){
 		str = str + 1;
