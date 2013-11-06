2c2da9480640b7317e180c84d16dc0180ae33cb7
l-tn3314 <l.tn3314@gmail.com>
2013-11-05 23:18:48 -0500

    fixed compile errors

--- Stuyablo/2/group1/Character.java
+++ Stuyablo/2/group1/Character.java
@@ -6,7 +6,7 @@ public class Character {
     protected String name;
     protected String charclass="none";
     protected int health,maxHealth;
-    protected int dex,str,intelligence;
+    protected int dexterity,strength,intelligence;
     protected int points;
     protected int exp=0,lvl=1;
     protected int x,y;
@@ -78,7 +78,6 @@ public class Character {
 	health = maxHealth = strength;
     }
 
-<<<<<<< HEAD
     public void setChar() {
 	Scanner sc = new Scanner(System.in);
 	System.out.print("What would you like to be? Warrior? Blah?");
@@ -94,7 +93,6 @@ public class Character {
 	    setChar();
 	}
     }
-=======
     //SETCLASS IS DEFINITELY NOT THE RIGHT WAY TO DO THIS
     //
     //public void setWarrior() {
@@ -106,7 +104,6 @@ public class Character {
     //    System.out.println("Dexterity: " + dexterity);
     //    setStat();
     //}
->>>>>>> 62a3cda57438aa9c13ea33ff7b8bca6a52b810cd
 
     public void setStat() {
 	points = 8;
@@ -119,7 +116,7 @@ public class Character {
 	    add = points;
 	    System.out.printf("Only able to add %d points\n",add);
 	}
-        str = str + add;
+        strength = strength + add;
         points = points - add;
 
         System.out.println("You still have " + points + " skill points available");
@@ -130,7 +127,7 @@ public class Character {
 	    System.out.printf("Only able to add %d points\n",add);
 	}
 
-	dex = dex + add;
+	dexterity = dexterity + add;
 	points = points - add;
 	
 	System.out.printf("%d points put into dexterity\n",add);
