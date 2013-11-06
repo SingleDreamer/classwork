1380442f208874cfbdc4b2ad7ea6240f24be8c56
Victor Gaitour <Grimlon1234@gmail.com>
2013-11-05 22:31:46 -0500

    Stuffs

--- Stuyablo/2/groupJJJD/Warrior.java
+++ Stuyablo/2/groupJJJD/Warrior.java
@@ -13,4 +13,16 @@ public class Warrior  extends Character{
     public String toString() {
 	return super.toString()+" the Warrior";
     }
+    /*    public void attack(){
+	rollDice();
+	if (roll > this.dexterity) {
+	    
+	    other.health = other.health - (int)this.dmg;
+	    System.out.println("You smacked him in the head with your giant mace");
+	    System.out.println(this.toString() + "'s Health: " + this.health);
+	    System.out.println(other.toString() + "'s Health: " + other.health);
+	    System.out.println(this.toString() + "'s Damage: " + this.dmg);
+	    System.out.println(other.toString() + "'s Damage: " + other.dmg);
+	}
+	}*/
 }
