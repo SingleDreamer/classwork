dc60863dcb9b86d42caf9da5119802a6f4205b13
Angela <angela97lin@gmail.com>
2013-11-05 19:31:15 -0500

    added combat and attack

--- Stuyablo/3/Group5/Driver.java
+++ Stuyablo/3/Group5/Driver.java
@@ -2,10 +2,21 @@ public class Driver {
     public static void main(String[] args) {
 	Goblin c = new Goblin("Bob");
 	Goblin a = new Goblin("Gob");
-	while (c.encounter(c,a) == 5){
-	    System.out.println(c.getHealth());
-	    System.out.println(a.getHealth());
-
+	
+	int battle = 0;
+	while (battle < 20){
+	    while (c.encounter(c,a) == 5){
+		System.out.println(c.getHealth());
+		System.out.println(a.getHealth());
+		c.level();
+		a.level();
+		System.out.println(c.getLevel());
+		System.out.println(a.getLevel());
+	    }
+	    c.health = c.maxhealth;
+	    a.health = a.maxhealth;
+	    battle ++;
 	}
     }
 }
+
