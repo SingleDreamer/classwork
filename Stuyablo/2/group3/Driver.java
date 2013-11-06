8c504e198c114f8dca0cd2e3f733802f8ec41bd3
Danny Qiu <dqiu55@gmail.com>
2013-11-06 02:23:37 -0500

    Merge everything together

--- Stuyablo/2/group3/Driver.java
+++ Stuyablo/2/group3/Driver.java
@@ -48,46 +48,50 @@ public class Driver {
 	}
 
         while (player.health > 0) {
-	    String map = new String();
-	    containsI p = new containsI();
+            String map = new String();
+            containsI p = new containsI();
 
-	    for ( int i = 0 ; i < xArray.length ; i++ )
-		System.out.println ( xArray[i] + ", " + yArray [ i ] );
+            for ( int i = 0 ; i < xArray.length ; i++ )
+            System.out.println ( xArray[i] + ", " + yArray [ i ] );
 
-	    for ( int k = 10 ; k >= -10 ; k-- ) {
-		if ( p.containsInt ( yArray, k ) ) {
-		    int index = p.findInt ( yArray ,k );
-		    int other = xArray [ index ];
-		    for ( int l = -10 ; l < other ; l++ ) {
-			map = map + "-";
-		    }
-		    map = map + "E";
-		    for ( int m = other + 1 ; m <= 10 ; m++ ) {
-			map = map + "-";
-		    }
-		}
-		else {
-		    for ( int i = -10 ; i <= 10 ; i++ ) {
-		        map = map + "-";
-		    }
-		}
-		map = map + "\n";
-	    }
-	    System.out.println ( player.xcor + ", " + player.ycor );
-	    //System.out.println ( map );
-	    int row = 10 - player.ycor;
-	    int column = 11 + player.xcor;
-	    String map1 = map.substring ( 0 , row * 22 + column - 1 );
-	    String map2 = map.substring ( row * 22 + column );
-	    System.out.println ( map1 + "Y" + map2 );
-	    System.out.println ( map.substring ( 1 , 25 ) );
-	    if ( player.exp >= 50 + Math.pow ( 2 , player.level ) ) {
-		player.level = player.level + 1;
-		player.exp = 0;
-		System.out.println ( "Congratulations! You have leveled up to level " + player.level );
-	    }
-            System.out.println ( "What would you like to do? (Attack nearest enemy(a), explore(e), or talk to friend(t)): ");
+            for ( int k = 10 ; k >= -10 ; k-- ) {
+            if ( p.containsInt ( yArray, k ) ) {
+                int index = p.findInt ( yArray ,k );
+                int other = xArray [ index ];
+                for ( int l = -10 ; l < other ; l++ ) {
+                map = map + "-";
+                }
+                map = map + "E";
+                for ( int m = other + 1 ; m <= 10 ; m++ ) {
+                map = map + "-";
+                }
+            }
+            else {
+                for ( int i = -10 ; i <= 10 ; i++ ) {
+                    map = map + "-";
+                }
+            }
+            map = map + "\n";
+            }
+            System.out.println ( player.xcor + ", " + player.ycor );
+            //System.out.println ( map );
+            int row = 10 - player.ycor;
+            int column = 11 + player.xcor;
+            String map1 = map.substring ( 0 , row * 22 + column - 1 );
+            String map2 = map.substring ( row * 22 + column );
+            System.out.println ( map1 + "Y" + map2 );
+            System.out.println ( map.substring ( 1 , 25 ) );
+
+            if (player.exp >= (50 + Math.pow(2, player.level))) {
+                player.level = player.level + 1;
+                player.exp = 0;
+                player.skills += 2;
+                System.out.println ( "Congratulations! You have leveled up to level " + player.level );
+            }
+
+            System.out.println(String.format("What would you line to do? (Attack nearest enemy(a), explore(e), get info(i), or distribue %d skills(d) ): ", player.skills));
             String choice = sc.nextLine();
+
             if (choice.equalsIgnoreCase("e")) {
                 System.out.println("Which direction would you like to go? (up, down, left, right)");
                 String dir = sc.nextLine();
@@ -119,10 +123,69 @@ public class Driver {
                 else
                     System.out.println ( "Misspelled direction" );
             }
-            else if (choice.equalsIgnoreCase("a"))
-                player.encounter();
+
+            else if (choice.equalsIgnoreCase("a")) {
+                if (npc.length <= 1) {
+                    npc = new Character[r.nextInt(6) + 5];
+                    for (int i=0; i<npc.length-1; i++) {
+                        npc[i] = new Ogre("Ogre " + i, player);
+                    }
+                    npc[npc.length-1] = new MrMoran("BOSS: MR.MORAN", player);
+                }
+                player.encounter(npc);
+                while (player.getEnemy().health > 0) {
+                    player.attack();
+                    player.getEnemy().attack();
+                }
+                Character[] tempNpc = new Character[npc.length - 1];
+                int offset = 0;
+                for (int i=0; i<tempNpc.length; i++) {
+                    if (npc[i].equals(player.getEnemy())){
+                        offset -= 1;
+                    }
+                    else{
+                        tempNpc[i + offset] = npc[i];
+                   }
+                }
+                npc = tempNpc;
+                player.getEnemy().die(player);
+            }
+            else if (choice.equalsIgnoreCase("i")) {
+                System.out.println(String.format("Class: %s\nLevel: %d\nExperience: %d/%d\nHealth: %d\nStrength: %d\nDexterity: %d\nIntelligence %d\n", player.charClass, player.level, player.exp, (int) (50 + Math.pow(2, player.level)), player.health, player.str, player.dex, player.iq));
+            }
+
+            else if (choice.equalsIgnoreCase("d")) {
+                int add;
+                System.out.println(String.format("You have %d skill points to distribute amongst your strength, dexterity, and intelligence.", player.skills));
+                System.out.println(String.format("How much would you like to add to your %d strength? %d skill points remaining.", player.str, player.skills));
+                add = sc.nextInt();
+                while (add > player.skills) {
+                    System.out.println(String.format("You don't have %d skill points to spend! Choose again.", add));
+                    add = sc.nextInt();
+                }
+                player.skills -= add;
+                player.str += add;
+                System.out.println(String.format("How much would you like to add to your %d dexterity? %d skill points remaining.", player.dex, player.skills));
+                add = sc.nextInt();
+                while (add > player.skills) {
+                    System.out.println(String.format("You don't have %d skill points to spend! Choose again", add));
+                    add = sc.nextInt();
+                }
+                player.skills -= add;
+                player.dex += add;
+                System.out.println(String.format("How much would you like to add to your %d intelligence? %d skill points remaining.", player.iq, player.skills));
+                add = sc.nextInt();
+                while (add > player.skills) {
+                    System.out.println(String.format("You don't have %d skill points to spend! Choose again", add));
+                    add = sc.nextInt();
+                }
+                player.skills -= add;
+                player.iq += add;
+            }
+
             else
                 System.out.println ( "Please enter a valid command" );
         }
+        System.out.println(String.format("\nOH NOES! YOU HAVE DIED! :(\nYour legacy as a level %d %s will stay in our hearts. <3", player.level, player.charClass));
     }
 }
