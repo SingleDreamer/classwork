b0ee6b00cf5f9c513d4d124d94467eb8901c948f
Danny Qiu <dqiu55@gmail.com>
2013-11-05 23:40:34 -0500

    Add loot to NPCs

--- Stuyablo/2/group3/MrMoran.java
+++ Stuyablo/2/group3/MrMoran.java
@@ -2,6 +2,34 @@ import java.util.*;
 
 public class MrMoran extends Character {
 
-    public MrMoran() {
+    private boolean loot; // true for weapon, false for armor
+
+    Random r = new Random(); 
+
+    public MrMoran(String n, Character player) {
+        super(n);
+        npcInit(player);
+        loot = r.nextBoolean();
+    }
+
+    public void die(Character player) {
+        int playerWeaponsL = player.weapons.length;
+        int playerArmorsL = player.armors.length;
+        if (loot) {
+            int[] tempWeapons = new int[playerWeaponsL];
+            for (int i=0; i<playerWeaponsL; i++) {
+                tempWeapons[i] = player.weapons[i];
+            }
+            tempWeapons[playerWeaponsL] = r.nextInt((int) (player.level * 1.5));
+            player.weapons = tempWeapons;
+        }
+        else {
+            int[] tempArmors = new int[playerArmorsL];
+            for (int i=0; i<playerArmorsL; i++) {
+                tempArmors[i] = player.armors[i];
+            }
+            tempArmors[playerArmorsL] = r.nextInt((int) (player.level + 2));
+            player.armors = tempArmors;
+        }
     }
 }
