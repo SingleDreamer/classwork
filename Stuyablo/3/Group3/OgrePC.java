516d0f91519307f2cbc103072f01d098ca78b26d
David <davidc83097@gmail.com>
2013-11-06 00:08:25 -0500

    updated

--- Stuyablo/3/Group3/OgrePC.java
+++ Stuyablo/3/Group3/OgrePC.java
@@ -11,10 +11,10 @@ protected int attackRange;
 public OgrePC() {
 	strength = 15;
 	health = strength;
-	maxhealth = 10;
+	maxhealth = health;
 	dexterity = 4;
 	attackRange = 5;
-	experience = 6;
+	experience = 0;
 	distance = generator.nextInt(10) + 1;
 }
 
@@ -33,23 +33,6 @@ public int DieRoll(){
 	return dieRoll;
 }
 
-public boolean flee(Character other){
-		DieRoll();
-		//if Ogre rolls less that dexterity, it gets away!!
-		if (dieRoll <= dexterity) {
-			System.out.println("The Ogre just fled");
-			dexterity = dexterity + 1;
-			experience = experience + 1;
-			return true;
-			isHere = false;
-		}
-		else {
-			System.out.println("The enamy tried to flee but wasn't fast enough!");
-			dexterity = dexterity - 1;
-			return false;
-		}
-}
-
 
 
 public void attack(Character other){
@@ -59,18 +42,13 @@ public void attack(Character other){
 		
 		//If Ogre roles less than its strength, it hits the attack!
 		if (dieRoll <= rnd) {
-			System.out.println ("You tried to attack, but failed!");
-			experience = experience - 1;
-			other.strength = other.strength + 1;
-			other.health = other.health + 1;
-			other.dexterity = other.dexterity + 1;
+			System.out.println ("You successfully attacked!");
+			other.health = other.health - 1;
+			experience = experience + 1;
 		}
 		//If Ogre roles more than its strength, it !
 		else {
-			System.out.println ("You succesfully attakced!");
-			experience = experience + 1;
-			strength = strength + 1; 
-			dexterity = dexterity + 1;
+			System.out.println ("You missed!");
 		}
 }
 
