105633c7afa6818ec1f2edfa4aa12f1a377f3c65
sapphacs13 <sappha.omeara@gmail.com>
2013-11-05 23:02:31 -0500

    Added Warrior

---
+++ Stuyablo/1/3-Group/Warrior.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Warrior extends PC {
+    
+    public Warrior() {
+	super();
+    }
+    
+    public void attack(Character other) {
+	super.attack();
+    }
+}
