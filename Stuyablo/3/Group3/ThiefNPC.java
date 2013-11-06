4fef10765bd1b626f5e1b258eee665c029b51905
stonemoore <nmeanims@ymail.com>
2013-11-06 01:59:37 -0500

    Update ThiefNPC.java

--- Stuyablo/3/Group3/ThiefNPC.java
+++ Stuyablo/3/Group3/ThiefNPC.java
@@ -17,7 +17,6 @@ public ThiefNPC() {
 
 
 public void attack(Character other){
-<<<<<<< HEAD
         int dice = DieRoll();
                 //If the distance is greater than the attack range, move closer by one
                 if (attackRange < distance) {
@@ -34,32 +33,7 @@ public void attack(Character other){
                         else {
                                         System.out.println ("The theif tried attacking you but missed!");
                         }
-                }
-	}
-=======
-	int dice = DieRoll();
-		//If the distance is greater than the attack range, move closer by one
-		if (attackRange < distance) {
-			distance = distance - 1;
-			}
-		else {
-			//If thief roles less than its dexterity, it hits the attack!
-			if (dice <= dexterity) {
-				System.out.println ("You just got attacked by the thief!");
-				experience = experience + 1;
-				other.health = other.health - 1;
-				}
-			//If thief roles more than its dexterity, it misses the attack!
-			else {
-					System.out.println ("The theif tried attacking you but missed!");
-			}
-		}
+      	}
 }
->>>>>>> 3e98ff56985fd5fcca2bf7a00f578a824ad077ac
-
-public String getName() {
-	return "thief";
-}
-
 
 }
