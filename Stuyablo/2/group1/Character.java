db89d8fc562649a6f89c2ebad6867bf6bf8fc888
ericwongg <wonggeric@gmail.com>
2013-11-06 18:44:18 -0500

    Optimize and added some new methods

--- Stuyablo/2/group1/Character.java
+++ Stuyablo/2/group1/Character.java
@@ -11,30 +11,165 @@ public class Character {
     protected int exp=0,lvl=1;
     protected int x,y;
 
-    public String getName() {
-	return name;
+    public void setChar() {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("What would you like to be?");
+	System.out.println("(1)Warrior or (2)Wizard");
+	int num = sc.nextInt();
+	if (num == 1) {
+	    System.out.println("Woo, You're a warrior");
+	    Warrior w = new Warrior(name);
+	}
+	else if (num == 2) {
+	    System.out.println("Woo, you're a Wizard");
+	    Wizard w = new Wizard(name);
+	}
+	else {
+	    System.out.println("Silly you, ponies aren't a choice");
+	    setChar();
+	}
     }
-	   
+
+    public void setStat(int n) {
+	points = n;
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("You have " + points + " skill points available.");
+	System.out.print("Strength = 8 + ");
+	int add = sc.nextInt();
+	if (add > points){
+	    add = points;
+	    System.out.printf("Only able to add %d points\n",add);
+	}
+        strength = strength + add;
+	health = strength;
+        points = points - add;
+
+	System.out.println("-------------------------------");
+
+        System.out.println("You still have " + points + " skill points available");
+	System.out.print("Dexterity = 8 + ");
+	add = sc.nextInt();
+	if (add > points) {
+	    add = points;
+	    System.out.printf("Only able to add %d points\n",add);
+	}
+
+	dexterity = dexterity + add;
+	points = points - add;
+	
+	System.out.println("-------------------------------");
+
+	System.out.printf("Remaining %d points put into intelligence\n",points);
+	intelligence += points;
+
+	System.out.println("-------------------------------");
+    }
+  
+    public boolean turn() {
+	if (exp > 100) {
+	    levelUp();
+	    turn();
+	}
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Hey you, what does your heart desire?");
+	System.out.println("(1)Fight or (2) Heal or (3) Quit");
+	int ans = sc.nextInt();
+
+	if (ans == 1) {
+	    encounter();
+	    turn();
+	}
+	else if (ans == 2) {
+	    heal();
+	    turn();
+	}
+	else if (ans == 3) {
+	    System.out.println("You are a disgrace.");
+	    return false;
+	}
+	else {
+	    System.out.println("Silly " + name + " that's not a choice.");
+	    turn();
+	}
+	return true;
+    }
+
+    public void encounter() {
+	Character enemy = new Character();
+	Random r = new Random();
+	if (r.nextDouble() > 0.5){
+	    enemy = new Ogre();
+	}
+	else{
+	    enemy = new Undead();
+	}
+	
+	while ((this.alive()) && (enemy.alive())){
+	    System.out.println("It's "+ enemy +"! \n What will you do?: ");	    
+	    System.out.print("(1)Fight or (2)Flee\n");
+	    Scanner sc = new Scanner(System.in);
+	    String input = sc.nextLine();
+	    if (input.equals("1")){
+		this.attack(enemy);
+		enemy.attack(this);
+		}
+	    else if (input.equals("2")){
+		this.flee();
+	    }
+	    else {
+		System.out.println("Invalid choice");
+	    }
+	    System.out.println("Your Health: " + this.getHealth());
+	    System.out.println("Enemy Health: " + enemy.getHealth());
+	    System.out.println("-------------------------------");
+	}
+	System.out.println("Someone died...");
+    }
+
+    public void heal() {
+	if (health == maxHealth) {
+	    System.out.println("You don't need a rest...");
+	    turn();
+	}
+	else {
+	    int add = (int)(maxHealth * .1);
+	    health = health + add;
+	    if (health > maxHealth) {
+		health = maxHealth;
+	    }
+	    System.out.println("You have been healed.");
+	}
+    }
+
+    public void levelUp() {
+	exp = 0;
+	lvl = lvl + 1;
+	System.out.println("Congratulations, you have grown so much wiser.");
+	System.out.println("As your reward, you have three skill points to add.");
+	setStat(3);
+	health = maxHealth = strength;
+    }
+
     public void attack(Character other) {
         System.out.println("Sissy Slap!");
     }
     
     public void takeDamage(int amount) {
-	    health -= amount;
-	    if(health < 0)
-		    health = 0;
+	health -= amount;
+	if(health < 0)
+	    health = 0;
     }
 
     public boolean flee() {
-	    Random r = new Random();
-	    if(r.nextFloat() > 0.35)
-	    {
-		    System.out.println("Got away safely");
-		    return true;
-	    }
-	    System.out.println("Failed to escape");
-	    return false;
-	
+	Random r = new Random();
+	if(r.nextFloat() > 0.35){
+	    System.out.println("Got away safely");
+	    return true;
+	}
+	System.out.println("Failed to escape");
+	return false;
     }
 
     public boolean hit() {
@@ -45,27 +180,6 @@ public class Character {
 	d3 = r.nextInt(6) + 1;
 	sum = d1 + d2 + d3;
 	return (sum <= dexterity);
-    }
-
-    public void encounter(Character other) {
-        while (other.getHealth() > 0 && this.getHealth() > 0){
-	    System.out.print("(1)Fight or (2)Flee\n");
-	    Scanner sc = new Scanner(System.in);
-	    String input = sc.nextLine();
-	    if (input.equals("1")){
-		this.attack(other);
-		other.attack(this);
-		}
-	    else if (input.equals("2")){
-		this.flee();
-	    }
-	    else {
-		System.out.println("Invalid choice");
-	    }
-	    System.out.println("Your Health: " + this.getHealth());
-	    System.out.println("Enemy Health: " + other.getHealth());
-	    System.out.println("-------------------------------");
-	}
     }
 
     /*
@@ -83,91 +197,6 @@ public class Character {
         return name;
     }
 
-    /* public Character() {
-	Scanner sc = new Scanner(System.in);
-	System.out.print("Welcome to StuyabloII.\nEnter your name: ");
-	name = sc.nextLine();
-	System.out.println("Hello " + name);
-	strength = dexterity = intelligence = 8;
-	setStat();
-	health = maxHealth = strength;
-    }
-    */
-
-    public void setChar() {
-	Scanner sc = new Scanner(System.in);
-<<<<<<< HEAD
-	System.out.print("What would you like to be? Warrior? Wizard? ");
-	if (sc.nextLine().equals("Warrior")) {
-=======
-	System.out.print("What would you like to be? Warrior? Wizard?\n");
-	String input = sc.nextLine();
-	if (input.equals("Warrior")) {
-	    Warrior w = new Warrior(name);
->>>>>>> ef390c44a389a69826bae6a12f99a52ebb10f42d
-	    System.out.println("Woo, You're a warrior");
-	    Warrior w = new Warrior(name);
-	}
-<<<<<<< HEAD
-	else if (sc.nextLine().equals("Wizard")) {
-=======
-	else if (input.equals("Wizard")) {
-	    Wizard w = new Wizard(name);
->>>>>>> ef390c44a389a69826bae6a12f99a52ebb10f42d
-	    System.out.println("Woo, you're a Wizard");
-	    Wizard w = new Wizard(name);
-	}
-	else {
-	    System.out.println("Silly you, ponies aren't a choice");
-	    setChar();
-	}
-    }
-
-    //SETCLASS IS DEFINITELY NOT THE RIGHT WAY TO DO THIS
-    //
-    //public void setWarrior() {
-    //    strength = 12;
-    //    dexterity = 4;
-    //    health = strength;
-    //    Scanner sc = new Scanner(System.in);
-    //    System.out.println("Strength: " + strength);
-    //    System.out.println("Dexterity: " + dexterity);
-    //    setStat();
-    //}
-
-
-    public void setStat() {
-	points = 8;
-	Scanner sc = new Scanner(System.in);
-
-	System.out.println("You have " + points + " skill points available.");
-	System.out.print("Strength = 8 + :\n");
-	int add = sc.nextInt();
-	if (add > points){
-	    add = points;
-	    System.out.printf("Only able to add %d points\n",add);
-	}
-        strength = strength + add;
-	health = strength;
-        points = points - add;
-	System.out.println("-------------------------------");
-        System.out.println("You still have " + points + " skill points available");
-	System.out.print("Dexterity = 8 + :\n");
-	add = sc.nextInt();
-	if (add > points) {
-	    add = points;
-	    System.out.printf("Only able to add %d points\n",add);
-	}
-
-	dexterity = dexterity + add;
-	points = points - add;
-	
-	System.out.printf("%d points put into dexterity\n",add);
-	System.out.println("-------------------------------");
-	System.out.printf("Remaining %d points put into intelligence\n",points);
-	intelligence += points;
-    }
-
     public int getHealth() {
 	return health;
     }
@@ -176,29 +205,6 @@ public class Character {
     {
 	    return health > 0;
     }
-
-    public int aliveState(Character other)
-    {
-	    if(alive())
-	    {
-		if(other.alive())
-			return 5;
-		else
-			return 3;
-	    }
-	    else
-	    {
-		if(other.alive())
-			return 2;
-	    	else
-			return 4;
-	    }
-    }
-
-    public String getCharclass() {
-	return charclass;
-    }
-
 
 }
 
--- Stuyablo/2/group1/Driver.java
+++ Stuyablo/2/group1/Driver.java
@@ -9,43 +9,16 @@ public class Driver {
 	Random r = new Random();
 
 	Scanner sc = new Scanner(System.in);
-	System.out.print("Welcome to StuyabloII. \n Enter your name: ");
+	System.out.print("Welcome to StuyabloII. \nEnter your name: ");
 	String name = sc.nextLine();
 	System.out.println("Hello " + name);
 	System.out.println("-------------------------------");
 	
 	player.setChar();
 
-	if (r.nextDouble() > 0.5){
-	    enemy = new Ogre();}
-	else{
-	    enemy = new Undead();}
-	
-
-	System.out.println("-------------------------------");
-
-	System.out.println(player.getHealth());
-	System.out.println(enemy.getHealth());
-	
-	while ((player.alive()) && (enemy.alive())){
-	    System.out.println("It's "+ enemy +"! \n What will you do?: ");	    
-	    System.out.print("(1)Fight or (2)Flee\n");
-	    String input = sc.nextLine();
-	    if (input.equals("1")){
-		player.attack(enemy);
-		enemy.attack(player);
-		}
-	    else if (input.equals("2")){
-		player.flee();
-	    }
-	    else {
-		System.out.println("Invalid choice");
-	    }
-	    System.out.println("Your Health: " + player.getHealth());
-	    System.out.println("Enemy Health: " + enemy.getHealth());
-	    System.out.println("-------------------------------");
+	while (true) {
+	    player.turn();
 	}
-	System.out.println("Someone died...");
     }
 }
 	       
