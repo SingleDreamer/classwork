d73cecca67cd749cc07e35b512338532815e43e7
Kevin Kan <kingkan97@gmail.com>
2013-11-06 00:13:02 -0500

    added random class and enemy

--- Stuyablo/2/group1/Driver.java
+++ Stuyablo/2/group1/Driver.java
@@ -3,34 +3,43 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args) {
+	Random r = new Random();
 	/*Character a,b;
 	a = new Character();
 	b = new Character();
 	a.attack(b);
 	*/
 	Scanner sc = new Scanner(System.in);
-	System.out.print("Welcome to StuyabloII. \nEnter your name: ");
+	System.out.print("Welcome to StuyabloII. \n Enter your name: ");
 	String name = sc.nextLine();
 	System.out.println("Hello " + name);
 	System.out.println("-------------------------------");
 	
-	Character player = new Warrior();
+	if (r.nextDouble > 0.5){
+	    Character player = new Warrior();}
+	else{
+	    Character player = new Wizard();}
+	if (r.nextDouble > 0.5){
+	    Character enemy = new Ogre();}
+	else{
+	    Character enemy = new Undead();}
+	
 
-	Character enemy = new Ogre();
 	System.out.println("-------------------------------");
-	System.out.println("It's an ogre! \nEnemy health: " + enemy.getHealth() + "\nWhat will you do?: ");
+	System.out.println("It's "+ enemy.toString +"! \n What will you do?: ");
 	while (enemy.getHealth() > 0 && player.getHealth() > 0){
-	    System.out.print("Fight(1)/Flee(2)\n");
+	    System.out.print("Fight/Flee\n");
 	    String input = sc.nextLine();
-	    if (input.equals("1")){
+	    if (input.equalsIgnoreCase("Fight")){
 		player.attack(enemy);
 		}
-	    else if (input.equals("2")){
+	    else if (input.equalsIgnoreCase("Flee")){
 		System.out.println("You coward...");
 	    }
 	    else
 		System.out.println("Invalid choice");
-	    System.out.println("Enemy health: " + enemy.getHealth());
+	    System.out.println("Your Health: " + player.getHealth());
+	    System.out.println("Enemy Health: " + enemy.getHealth());
 	    System.out.println("-------------------------------");
 	}
 	System.out.println("Someone died...");
