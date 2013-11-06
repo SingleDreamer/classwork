259ab298ebda16805e82ab7fe485de29bf52c91f
Vanessa <yu100997@gmail.com>
2013-11-05 19:40:40 -0500

    update

--- Stuyablo/1/3-Group/PC.java
+++ Stuyablo/1/3-Group/PC.java
@@ -1,3 +1,19 @@
 import java.io.*;
 import java.util.*;
 
+public class PC extends Character{
+    
+    public void PCCreation() {
+	dexterity = 8;
+	strength = 8;
+	intelligence = 8;
+	int pointsleft = 8;
+	System.out.print("Enter Name: ");
+	Scanner x = new Scanner(System.in);
+	String n = x.nextLine();
+	name = n;
+
+        for (int x = 8
+    }
+
+}
