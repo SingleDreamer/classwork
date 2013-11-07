2d079b4ca7a68d06cc0cb1353ad274e2fd2a79e4
MaryRosariaTaft <Mary.Taft97@gmail.com>
2013-11-07 16:53:29 -0500

    Made minor modifications to the gameplay.

--- Stuyablo/3/Group6/Driver.java
+++ Stuyablo/3/Group6/Driver.java
@@ -19,22 +19,22 @@ public class Driver {
 	comment out the block you don't want to run.
 	*/
 
-	/*
+	
 	c.setAttributes();
 	w.Startup();
 	System.out.println(w.name + " faces " + c.name + "\n" + "Status:\n" + c.getStatus());
 	for (int i = 1, j = 0; w.getHealth() > 0 && c.getHealth() > 0 && j==0; i++) {
+	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
 	    System.out.println("Round " + i);
 	    int e1 = w.encounter(c);
 	    if (e1 == 0)
 		j = 1;
-	    System.out.println("\tUpdated Stats:");
 	    System.out.println(w.getStatus());
 	    System.out.println(c.getStatus());
 	    System.out.println();
 	    if (j==0 && c.health > 0){
 	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
-	    System.out.println("---NPC's Response---\n");
+	    System.out.println("---Ogre's Response---\n");
 	    int e2 = c.encounter(w);
 	    if (e2 == 0)
 		j = 1;
@@ -43,7 +43,7 @@ public class Driver {
 	    }
 	    System.out.println("------------------------");
 	}
-	*/
+	
 
 
 	/*
@@ -51,17 +51,17 @@ public class Driver {
 	t.Startup();
 	System.out.println(t.name + " faces " + c.name + "\n" + "Status:\n" + c.getStatus());
 	for (int i = 1, j = 0; t.getHealth() > 0 && c.getHealth() > 0 && j==0; i++) {
+	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
 	    System.out.println("Round " + i);
 	    int e1 = t.encounter(c);
 	    if (e1 == 0)
 		j = 1;
-	    System.out.println("\tUpdated Stats:");
 	    System.out.println(t.getStatus());
 	    System.out.println(c.getStatus());
 	    System.out.println();
 	    if (j==0 && c.health > 0){
 	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
-	    System.out.println("---NPC's Response---\n");
+	    System.out.println("---Ogre's Response---\n");
 	    int e2 = c.encounter(t);
 	    if (e2 == 0)
 		j = 1;
@@ -77,17 +77,17 @@ public class Driver {
 	w.Startup();
 	System.out.println(w.name + " faces " + m.name + "\n" + "Status:\n" + m.getStatus());
 	for (int i = 1, j = 0; w.getHealth() > 0 && m.getHealth() > 0 && j==0; i++) {
+	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
 	    System.out.println("Round " + i);
 	    int e1 = w.encounter(m);
 	    if (e1 == 0)
 		j = 1;
-	    System.out.println("\tUpdated Stats:");
 	    System.out.println(w.getStatus());
 	    System.out.println(m.getStatus());
 	    System.out.println();
 	    if (j==0 && m.health > 0){
 	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
-	    System.out.println("---NPC's Response---\n");
+	    System.out.println("---Goblin Mage's Response---\n");
 	    int e2 = m.encounter(w);
 	    if (e2 == 0)
 		j = 1;
@@ -98,14 +98,15 @@ public class Driver {
 	}
 	*/
 
+	/*
 	c.setAttributes();
 	System.out.println(m.name + " faces " + c.name + "\n" + "Status:\n" + m.getStatus()+"\n"+c.getStatus());
 	for (int i = 1, j = 0; m.getHealth() > 0 && c.getHealth() > 0 && j==0; i++) {
+	    try{Thread.sleep(2000);}catch(InterruptedException ex){}
 	    System.out.println("Round " + i);
 	    int e1 = m.encounter(c);
 	    if (e1 == 0)
 		j = 1;
-	    System.out.println("\tUpdated Stats:");
 	    System.out.println(m.getStatus());
 	    System.out.println(c.getStatus());
 	    System.out.println();
@@ -120,5 +121,8 @@ public class Driver {
 	    }
 	    System.out.println("------------------------");
 	}
+	*/
+
+
     }
 }
