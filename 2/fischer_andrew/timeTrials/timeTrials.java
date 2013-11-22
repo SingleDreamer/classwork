160118b5e0cce689cb643fdb2ff05c44c71d62e4
afischer15 <afischer15@mac.com>
2013-11-22 09:27:42 -0500

    time trials

--- 2/fischer_andrew/timeTrials/timeTrials.java
+++ 2/fischer_andrew/timeTrials/timeTrials.java
@@ -8,29 +8,30 @@ public class timeTrials{
 	ArrayList<Integer> a = new ArrayList<Integer>();
 	Random r = new Random();
 
-	for(int i = 0; i < 1001; i++){
+	for(int i = 0; i < 1000000; i++){
 	    a.add(r.nextInt(9999));
 	}
 
+
 	long start = System.currentTimeMillis();
 	a.add(1);
-	long stop = System.currentTimeMillis();
-	System.out.println("a.add took " + stop-start + "ms.");
+	long stop = System.currentTimeMillis()-start;
+	System.out.println("a.add took " + stop + "ms.");
 
 	long start2 = System.currentTimeMillis();
 	a.add(0,1);
-	long stop2 = System.currentTimeMillis();
-	System.out.println("a.add at end took " + stop2-start2 + " ms.");
+	long stop2 = System.currentTimeMillis()-start2;
+	System.out.println("a.add at end took " + stop2 + " ms.");
 
 	long start3 = System.currentTimeMillis();
 	a.remove(0);
-	long stop3 = System.currentTimeMillis();
-	System.out.println("a.remove(0) took " + stop3-start3 + " ms.");
+	long stop3 = System.currentTimeMillis()-start3;
+	System.out.println("a.remove(0) took " + stop3 + " ms.");
 
 	long start4 = System.currentTimeMillis();
 	a.remove(a.size()-1);
-	long stop4 = System.currentTimeMillis();
-	System.out.println("a.remove at end took " + stop4-start4 + " ms.");
+	long stop4 = System.currentTimeMillis()-start4;
+	System.out.println("a.remove at end took " + stop4 + " ms.");
 
     }
 }
