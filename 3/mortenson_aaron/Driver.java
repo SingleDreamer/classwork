49c4cc35c14aa49cf0b42ab55e3121afee45df1d
Aaron <aaron@ubuntu>
2013-11-24 00:58:59 -0500

    Completed arraylist work

--- 3/mortenson_aaron/Driver.java
+++ 3/mortenson_aaron/Driver.java
@@ -3,39 +3,28 @@ import java.io.*;
 
 public class Driver {
     public static void main(String args[]) {
-	ArrayList <String> a1 = new ArrayList<String>();
-	ArrayList <String> a2 = new ArrayList<String>();
-	long start = System.currentTimeMillis();
-	for (int i = 0;i<100000;i++) {
-	    a1.add("Moo");
+	/*	
+a.add(s) time for 100000:11
+a.add(0,s) time for 100000:7
+a.add(s) time for 20000000:220
+a.add(0,s) time for 20000000:567
+
+a.remove(0) time for 10000:17
+a.remove(a.length) time for 10000:0
+a.remove(0) time for 100000:933
+a.remove(a.length) time for 100000:0
+
+	*/
+	ArrayList <Integer> a = new ArrayList<Integer>();
+	for (int i=0;i<100;i++)
+	    a.add(i);
+	System.out.println(a);
+	for (int i=0;i<a.size()/2;i++) {
+	    int temp = a.get(i);
+	    a.set(i,a.get((a.size()-(1+i))));
+	    a.set((a.size()-(1+i)),temp);
 	}
-	long end = System.currentTimeMillis();
-	long time = end-start;
-	System.out.print("a.add(s) time for 100000:");
-	System.out.println(time);
-	start = System.currentTimeMillis();
-	for (int i = 0;i<100000;i++) {
-	    a2.add("Moo");
-	}
-	end = System.currentTimeMillis();
-	time = end-start;
-	System.out.print("a.add(0,s) time for 100000:");
-	System.out.println(time);
-	start = System.currentTimeMillis();
-	for (int i = 0;i<20000000;i++) {
-	    a1.add("Moo");
-	}
-	end = System.currentTimeMillis();
-	 time = end-start;
-	System.out.print("a.add(s) time for 20000000:");
-	System.out.println(time);
-	start = System.currentTimeMillis();
-	for (int i = 0;i<20000000;i++) {
-	    a2.add("Moo");
-	}
-	end = System.currentTimeMillis();
-	time = end-start;
-	System.out.print("a.add(0,s) time for 20000000:");
-	System.out.println(time);
+	System.out.println(a);
     }
+
 }
