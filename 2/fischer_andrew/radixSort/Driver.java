1b003d54341326118db35ec517730ce890a2043d
afischer15 <afischer15@mac.com>
2013-11-26 09:11:54 -0500

    boop

---
+++ 2/fischer_andrew/radixSort/Driver.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+
+
+public class Driver{
+    public static void main (String[] args){
+
+
+	
+	int[] buckets = new int[10]; //Array to be filled with array lists, each having rspective avlkuarweopifwefo
+
+	Random r = new Random();
+	for(i=0; i<10; i++){      //I is the positition in the arraylsit itself
+     
+	    ArrayList<integer>  = new ArrayList<integer>;
+	    buckets[i] = 
+
+
+	    for (j=0; j<21; j++){ //J is the position in the arrray in the array list
+		buckets[j].add(r.nextInt);
+	    }
+	}
