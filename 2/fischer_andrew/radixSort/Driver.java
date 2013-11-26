353a61aa11afffffcf92a4c0c56bfcec50ef1112
afischer15 <afischer15@mac.com>
2013-11-26 09:20:31 -0500

    bbblerrghhh

--- 2/fischer_andrew/radixSort/Driver.java
+++ 2/fischer_andrew/radixSort/Driver.java
@@ -7,17 +7,10 @@ public class Driver{
     public static void main (String[] args){
 
 
-	
-	int[] buckets = new int[10]; //Array to be filled with array lists, each having rspective avlkuarweopifwefo
+	ArrayList[] = new ArrayList[]
 
 	Random r = new Random();
+	
 	for(i=0; i<10; i++){      //I is the positition in the arraylsit itself
-     
-	    ArrayList<integer>  = new ArrayList<integer>;
-	    buckets[i] = 
-
-
-	    for (j=0; j<21; j++){ //J is the position in the arrray in the array list
-		buckets[j].add(r.nextInt);
-	    }
-	}
+              
+	    
