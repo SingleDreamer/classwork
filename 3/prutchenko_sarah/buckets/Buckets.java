710e3afee951ffb5dc57dfab8c264f4ad39f8729
sarah <sprutcheno@gmail.com>
2013-11-27 10:12:46 -0500

    still working on buckets...

--- 3/prutchenko_sarah/buckets/Buckets.java
+++ 3/prutchenko_sarah/buckets/Buckets.java
@@ -1,20 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Buckets{
-    
-    public class Buckets(){
 	ArrayList[] buckets= new ArrayList[10];
+    Random r = new Random();
+    public void makeRandom(){
 	for (int i=0; i<10; i++){
-	    buckets[i]= (int)Math.round(Math.random*9999);
+	    buckets[i].set(r.nextInt(10000));
 	}
     }
-	public void sort(){
-	    for(int n=0; n<4; n++){
-		ArrayList[] temp= new ArrayList[10];
-		for (int j=0; j<10; j++){
-		    temp.add((buckets[j]/math.pow(10. n)), buckets[j]);
-		}
-		buckets=temp;
+
+    public void sort(){
+	for(int n=0; n<4; n++){
+	    ArrayList[] temp= new ArrayList[10];
+	    for (int j=0; j<10; j++){
+		int exp=(int)Math.pow(10,n);
+		temp.add((buckets[j]/exp%10), buckets[j] );
 	    }
+	    buckets=temp;
 	}
+    }
 }
