b3393da59a17b88e5ffc122d5878b158c6b96ef4
MaryRosariaTaft <Mary.Taft97@gmail.com>
2013-11-28 20:24:17 -0500

    Made the sort method function properly.

--- 3/taft_mary/Radix/Driver.java
+++ 3/taft_mary/Radix/Driver.java
@@ -5,9 +5,10 @@ public class Driver {
 
     public static void main(String[] args) {
 	Radix r = new Radix(100,4);
-	System.out.println(r+"\n\n");
+	System.out.println("Original array:\n"+r);
+	System.out.println();
 	r.sort();
-	System.out.println(r);
+	System.out.println("Sorted array:\n"+r);
     }
 
 }
