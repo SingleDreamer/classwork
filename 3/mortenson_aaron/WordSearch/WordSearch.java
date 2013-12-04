be4a664239a3fc6f9fd5cc50d509c38eec0019dc
Aaron <aaron@ubuntu>
2013-12-03 20:49:23 -0500

    wordsearch pt 1

--- 3/mortenson_aaron/WordSearch/WordSearch.java
+++ 3/mortenson_aaron/WordSearch/WordSearch.java
@@ -27,12 +27,34 @@ public class WordSearch {
         }
         return s;
     }
+/*
+Precondition: row, col represent the location to try to place the
+	          first character of word
 
+              word represents the word to try to place in the puzzle
+
+Postcondition: If the word can be legally placed in the puzzle, modify
+	           the board array so that the word is in the puzzle and
+			   return True.
+			   Otherwise, return false
+	          
+*/
     public boolean addWordH(int row, int col, String word) {
 	int len = word.length();
 	int rows = board.length;
 	int cols = board[0].length;
-	return true;
+	boolean go = true;
+	if (col<=cols && row+len<=rows) {
+	    for (int i=row;i<row+len;i++) {
+		if (go)
+		    go = (board[col][i]==word.charAt(i-row) || board[col][i]=='-');
+	    }
+	    if (go) {
+		for (int i=row;i<row+len;i++)
+		    board[col][i]=word.charAt(i-row);
+	    }
+	}
+	return go;
 }
 
 }
