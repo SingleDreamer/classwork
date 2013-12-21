7a7f2d2598e9749b62182a18b1a1e5db8f75676f
MaryRosariaTaft <Mary.Taft97@gmail.com>
2013-12-20 19:27:16 -0500

    Added a comment.

--- 3/taft_mary/Subway/TrainLine.java
+++ 3/taft_mary/Subway/TrainLine.java
@@ -5,6 +5,7 @@ public class TrainLine {
 
     private String name;
     private ArrayList<Station> stops = new ArrayList<Station>();
+    //I used an ArrayList here for the purpose of making setup easier, but we could use an array
 
     public TrainLine(String name) {
 	this. name = name;
