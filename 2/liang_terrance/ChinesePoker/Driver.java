31ffb4775c686a0ab0cc96c3db4c3201cedb30fd
TLiang518 <terranceliang01@gmail.com>
2013-12-23 18:28:46 -0500

    Final Project

---
+++ 2/liang_terrance/ChinesePoker/Driver.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args){
+
+    Card s = new Card(3,4);
+    System.out.println(s);
+
+    }
+
+
+}
