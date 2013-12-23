881673a2c99df09d6e4c54e413227e746d1e72f8
TLiang518 <terranceliang01@gmail.com>
2013-12-23 18:34:30 -0500

    Update

--- 2/liang_terrance/ChinesePoker/ChinesePoker.java
+++ 2/liang_terrance/ChinesePoker/ChinesePoker.java
@@ -2,7 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class ChinesePoker{
-    
+    private Card[][] deck = new Card[4][13];
+
+    public ChinesePoker(){    
+	for (int i=0; i<4; i++){
+	    for (int j=0; j<13; j++){
+		deck[i][j]= new Card (i,j);
+	    }
+	}
+    }
 
 
 
