2777788ebfd3110cb60934e26f4e699c9375c5c7
Kemraj Sobhai <kbsobhai@gmail.com>
2014-01-03 14:47:20 -0500

    work on project

--- 1/sobhai_kemraj/Final_Project_Beta_1.0/MainBoard.java
+++ 1/sobhai_kemraj/Final_Project_Beta_1.0/MainBoard.java
@@ -1,19 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class MainBoard{
-	private char [][] board;
-	private boolean play;
-	private char P1, P2;
-	private String Player1, Player2;
+public class MainBoard extends Board{
 	
-	public MainBoard(){
-		//board = new char [3][3];
+    public MainBoard(String name1, String name2){
+		board = new char [3][3];
 		this.play = true;
 		P1 = 'X';
 		P2 = 'O';
-		//Player1 = name1;
-		//Player2 = name2;
+		Player1 = name1;
+		Player2 = name2;
 	}
 	
 	public void move(char player, int row, int col){
