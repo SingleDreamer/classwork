1e337af7d176b192fbafc1e7b9248645a8fd5952
Justin Weltz <jdnweltz@gmail.com>
2014-01-04 09:29:17 -0800

    Version 4

--- 1/weltz_justin/finalproject4/Gui.java
+++ 1/weltz_justin/finalproject4/Gui.java
@@ -18,12 +18,16 @@ public class Gui {
 	JComboBox questionInteriorColor = new JComboBox(questionInteriorColorStrings);
 	JComboBox questionPatterns = new JComboBox(questionPatternStrings);
 	questionPatterns.setPreferredSize(new Dimension(250,40));
-	JButton questionSubmit = new JButton("Submit Your Questions");
+	JButton questionSubmit = new JButton("Submit Your Question(s)");
 	questionSubmit.setPreferredSize(new Dimension (250,50));
+	JTextField guess = new JTextField("Guess here...");
+	guess.setPreferredSize(new Dimension(100,20));
+	JButton guessButton = new JButton("Guess");
+	guessButton.setPreferredSize(new Dimension(250,50));
         frame.setVisible(true);
         frame.setSize(938,800);
 	Container contentPane = frame.getContentPane();
-	JPanel questions = new JPanel(new FlowLayout(FlowLayout.CENTER,5,100));
+	JPanel questions = new JPanel(new FlowLayout(FlowLayout.CENTER,5,65));
 	questions.setBackground(Color.GREEN.darker());
 	questions.setVisible(true);
 	questions.setPreferredSize(new Dimension(290,800));
@@ -32,6 +36,8 @@ public class Gui {
 	questions.add(questionInteriorColor);
 	questions.add(questionPatterns);
 	questions.add(questionSubmit);
+	questions.add(guess);
+	questions.add(guessButton);
 	contentPane.setLayout(new FlowLayout(FlowLayout.LEADING));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         draw object = new draw();
